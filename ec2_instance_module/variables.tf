38f96e07b52bdf7e97fcb39e099dd684018a2ae5
Soukath <sokualee@gmai.com>
2025-08-17 14:03:06 +0530

    added ec2_instance_moduke

---
+++ ec2_instance_module/variables.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    default = "ami-0de716d6197524dd9"
+
+  
+}
+variable "instance_type" {
+    default = "t2.micro"
+
+  
+}
+variable "key_name" {
+    default = "b16key"
+    
+
+  
+}
+variable "instance_name" {
+    default = "wipro_server"
+  
+}
